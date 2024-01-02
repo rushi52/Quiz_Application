41a64d1466fc3f8fc3ec8e5e1964902db2c7aede
rushi52 <rushi.jammana@gmail.com>
2024-01-02 19:13:40 +0530

    Question Dao Layer

--- quiz/src/main/java/com/rushi/quiz/dao/QuestionDao.java
+++ quiz/src/main/java/com/rushi/quiz/dao/QuestionDao.java
@@ -4,18 +4,12 @@ import com.rushi.quiz.model.Question;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface QuestionDao extends MongoRepository<Question,Integer> {
 
-
     List<Question> findByCategory(String category);
-
-
-
-
 
     @Aggregation(pipeline = {
             "{ $match: { category : ?0 } }",
