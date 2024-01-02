8f2855f341c333d4eefd19cc5270fbc8f1fbb728
rushi52 <rushi.jammana@gmail.com>
2024-01-02 19:33:15 +0530

    Quiz monolith application

--- quiz/src/main/java/com/rushi/quiz/QuizApplication.java
+++ quiz/src/main/java/com/rushi/quiz/QuizApplication.java
@@ -1,10 +1,8 @@
 package com.rushi.quiz;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
 @SpringBootApplication
 public class QuizApplication {
 	public static void main(String[] args) {
