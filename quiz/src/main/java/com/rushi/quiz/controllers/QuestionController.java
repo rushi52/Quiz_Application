fdc54e9339b4cd0b9cd50dc00d2d29502c9ad4b7
rushi52 <rushi.jammana@gmail.com>
2024-01-02 19:08:23 +0530

    Question controller

--- quiz/src/main/java/com/rushi/quiz/controllers/QuestionController.java
+++ quiz/src/main/java/com/rushi/quiz/controllers/QuestionController.java
@@ -3,23 +3,18 @@ package com.rushi.quiz.controllers;
 import com.rushi.quiz.model.Question;
 import com.rushi.quiz.services.QuestoinService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/questions")
-//@RequestMapping("/")
+
 public class QuestionController {
     @Autowired
     QuestoinService questoinService;
 
     @GetMapping("/allQuestions")
-//    @GetMapping()
     public ResponseEntity<List<Question>> allQuestions(){
         return questoinService.allQuestionsService();
 
